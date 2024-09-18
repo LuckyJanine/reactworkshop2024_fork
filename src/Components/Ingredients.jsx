d6697ea35575b9f8c1fbcf1c66a7149d4eefcb4c
LJ_Tao <lingjie.tao63@gmail.com>
2024-09-18 22:04:55 +0200

    refactor css

--- src/Components/Ingredients.jsx
+++ src/Components/Ingredients.jsx
@@ -1,21 +1,17 @@
 import React from "react";
-import IngredientDetail from "./IngredientDetail";
 
 const Ingredients = (props) => {
+
     const ingredients = props.ingredients;
-    const flip = props.flip;
 
     return ( 
-        <div className="ingrediant-modal-container">
-            <div className="modal-content">
-                {/* <p className="modal-close" onClick={flip}>
+        <div className="ingrediant-container">
+            {/* <p className="modal-close" onClick={flip}>
                     &times;
                 </p> */}
-                {ingredients.map((ingredient) => (
-                    <p>{ingredient.text}</p> 
-                ))}
-                
-            </div>
+            {ingredients.map((ingredient) => (
+                <p>{ingredient.text}</p> 
+            ))}
         </div>
      );
 }
